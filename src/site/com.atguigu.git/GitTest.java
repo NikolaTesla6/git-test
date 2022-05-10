0756d07eae59f24b483fa27b4dd8518bf4533df3
NikolaTesla <wanghongbin1997@gmail.com>
2022-05-10 14:03:33 +0800

    三次提交

--- src/site/com.atguigu.git/GitTest.java
+++ src/site/com.atguigu.git/GitTest.java
@@ -6,5 +6,6 @@ public class GitTest {
     public static void main(String[] args) {
         System.out.pringln("Hello Git");
         System.out.pringln("Hello Git2");
+        System.out.pringln("Hello Git3");
     }
 }
